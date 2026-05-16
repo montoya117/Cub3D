41676b8d6c92734a84ce7c42985bc0b06c8f7416
jadelgad <jadelgad@student.42barcelona.com>
2026-05-16 15:27:58 +0200

    raycasting

--- cub_3d_v1.1/src/graphics/draw.c
+++ cub_3d_v1.1/src/graphics/draw.c
@@ -1,8 +1,57 @@
 
 #include "cub_3d.h"
+#include <math.h>
+#define FOV (M_PI / 3)   // 60 grados
 
 void    draw(t_data *data)
 {
     draw_minimap_buffer(data);
     //ya anyadiremos mas cosas !! :3
+
+        // ----------- Prueba: columna vertical -----------
+    double dir_angle = data->player.dir; // dirección actual del jugador (en radianes)
+
+    for (int x = 0; x < WIN_W; x++)
+    {
+        double percent = (double)x / (double)WIN_W;
+        double ray_angle = dir_angle - (FOV / 2) + percent * FOV;
+
+        // Dirección del rayo
+        double dir_x = cos(ray_angle);
+        double dir_y = sin(ray_angle);
+
+        // Posición inicial del rayo (centro del jugador)
+        double ray_x = data->player.pos_x + 0.5;
+        double ray_y = data->player.pos_y + 0.5;
+
+        // Lanza el rayo paso a paso hasta chocar con un muro
+        double dist = 0.0, step = 0.01;
+        int hit = 0;
+        while (!hit && dist < 20.0)
+        {
+            ray_x += dir_x * step;
+            ray_y += dir_y * step;
+            dist += step;
+            int map_x = (int)ray_x;
+            int map_y = (int)ray_y;
+            if (data->map.grid[map_y][map_x] == '1')
+                hit = 1;
+        }
+
+        // Corrige el efecto "ojos de pez" (fisheye)
+        dist = dist * cos(ray_angle - dir_angle);
+
+        // Altura de la pared en pantalla
+        int line_height = (int)(WIN_H / dist);
+
+        // Dibuja la columna vertical en la pantalla
+        int y_start = WIN_H / 2 - line_height / 2;
+        int y_end = WIN_H / 2 + line_height / 2;
+
+        unsigned int color = 0xCCCCCC; // color pared
+        for (int y = y_start; y < y_end; y++)
+            if (y >= 0 && y < WIN_H)
+                buffer_put_pixel(&data->mlx, x, y, color);
+    }
+    // ------------------------------------------------
 }
--- cub_3d_v1.1/src/main.c
+++ cub_3d_v1.1/src/main.c
@@ -1,6 +1,32 @@
 
 #include "cub_3d.h"
 
+void hardcodear_mapa(t_data *data)
+{
+    // Tamaño del mapa
+    #define MAP_HEIGHT 5
+
+    // Mapa de ejemplo: el jugador está en (2, 2) mirando al norte
+    data->map.grid = malloc(sizeof(char *) * (MAP_HEIGHT + 1));
+    if (!data->map.grid)
+        return; // Maneja error si quieres
+
+    data->map.grid[0] = ft_strdup("1111111111");
+    data->map.grid[1] = ft_strdup("1000000001");
+    data->map.grid[2] = ft_strdup("10N0000001");
+    data->map.grid[3] = ft_strdup("1000000001");
+    data->map.grid[4] = ft_strdup("1111111111");
+    data->map.grid[5] = NULL;
+
+    data->map.height = MAP_HEIGHT;
+    data->map.width = 5; // todas las filas son iguales
+
+    // Posición y dirección del jugador (coincide con la 'N' en [2][2])
+    data->player.pos_x = 3;
+    data->player.pos_y = 3;
+    data->player.dir = 'N';
+}
+
 int main(int argc, char *argv[])
 {
     t_data  data;
@@ -20,6 +46,8 @@ int main(int argc, char *argv[])
     }
     // data_init
     init_data(&data);
+    //mientras no
+    hardcodear_mapa(&data);
     // parser
     if (parser_main(map_file, &data) != 0)
     {
