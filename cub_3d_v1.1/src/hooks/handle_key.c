bb452f5877173e09e54b642d356552b69d5aac32
jadelgad <jadelgad@student.42barcelona.com>
2026-05-21 14:29:55 +0200

    colisiones

--- cub_3d_v1.1/src/hooks/handle_key.c
+++ cub_3d_v1.1/src/hooks/handle_key.c
@@ -1,16 +1,41 @@
 #include "cub_3d.h"
 
+void check_and_move(int new_x, int new_y, t_data *data)
+{
+	char	cell;
+
+	cell = data->map.grid[new_y][new_x];
+	if (new_x >= 0 && new_x < data->map.width &&
+		new_y >= 0 && new_y < data->map.height)
+		{
+			if (cell == '0' || cell == 'N' ||
+				cell == 'S' || cell == 'E' || cell == 'W')
+			{
+				data->player.pos_x = new_x;	
+				data->player.pos_y = new_y;
+			}
+		}
+}
+
+
 int	handle_keypress(int keycode, t_data *data)
 {
+	int	new_x;
+	int	new_y;
+
+	new_x = data->player.pos_x;
+	new_y = data->player.pos_y;
+
 	if (keycode == 65307) // ESC
 		exit(0);
 	if (keycode == 119) // W
-		data->player.pos_y -= 1;
+		new_y -= 1;
 	else if (keycode == 115) // S
-		data->player.pos_y += 1;
+		new_y += 1;
 	else if (keycode == 97) // A
-		data->player.pos_x -= 1;
+		new_x -= 1;
 	else if (keycode == 100) // D
-		data->player.pos_x += 1;
-	return (0);
+		new_x += 1;
+	check_and_move(new_x, new_y, data);
+		return (0);
 }
